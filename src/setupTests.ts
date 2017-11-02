82b1777e0351d8a8fd8c351b210dd28f24e2e89a
Kyle Bremner <kyle@defining.tech>
2017-11-02 17:01:29 +0000

    Enables synthetic default imports

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,8 +2,6 @@ import 'jest-enzyme';
 // see comments in this file for more details about why it is required
 import './shim';
 import { configure } from 'enzyme';
-// see https://github.com/airbnb/enzyme/pull/1264 for more info behind
-// why we need to import the constructor like this
-import * as Adapter from 'enzyme-adapter-react-16';
+import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
