7ef2936578273c5bcfe877520e5015f0dcb49b0b
Kyle Bremner <kyle@defining.tech>
2017-11-03 09:07:27 +0000

    Adds basic styling to CakeCard

--- src/components/cakeCard/cakeCard.tsx
+++ src/components/cakeCard/cakeCard.tsx
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
+import './cakeCard.css';
 import Cake from '../../models/cake';
+
+const defaultCakeImage = require('./default_cake.png');
 
 class CakeCard extends Component<Cake> {
     render() {
         const { imageUrl, name } = this.props;
         return (
-            <div>
-                <img src={imageUrl} />
+            <div className="cakeCard">
+                <img src={imageUrl || defaultCakeImage} onError={this.onImageLoadError} />
                 <span>{name}</span>
             </div>
         );
+    }
+
+    onImageLoadError(event: React.SyntheticEvent<HTMLImageElement>) {
+        if (event.currentTarget.src !== defaultCakeImage) {
+            event.currentTarget.src = defaultCakeImage;
+        }
     }
 }
 
