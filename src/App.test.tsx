209cf3346cc1683b3a8c11cd82745c92cbf8e19a
Kyle Bremner <kyle@defining.tech>
2017-11-03 15:41:21 +0000

    Fixes render test for App component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import createHistory from 'history/createBrowserHistory';
+import { MemoryRouter } from 'react-router';
 import configureStore from './store';
 import App from './App';
 
 it('renders without crashing', () => {
-  const history = createHistory();
-  const store = configureStore(history);
-  const div = document.createElement('div');
-  ReactDOM.render(<Provider store={store}><App /></Provider>, div);
+    const history = createHistory();
+    const store = configureStore(history);
+    const div = document.createElement('div');
+    ReactDOM.render(
+        <Provider store={store}>
+            <MemoryRouter>
+                <App />
+            </MemoryRouter>
+        </Provider>
+    , div);
 });
